79fe23d5677288559aecea1c3f87bba53dcfc215
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2013-10-11 13:35:15 +0900

    add codes for gathering statistics

--- ryu/services/vrrp/manager.py
+++ ryu/services/vrrp/manager.py
@@ -136,6 +136,7 @@ class VRRPManager(app_manager.RyuApp):
         monitor.start()
         router.start()
         stats_out_timer.start(config.statistics_interval)
+        self.logger.info("stats interval : "+config.statistics_interval)
         self.logger.info("stats timer started")
         rep = vrrp_event.EventVRRPConfigReply(router.name, interface, config)
         self.reply_to_request(ev, rep)
