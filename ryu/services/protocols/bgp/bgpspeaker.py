e06b93040a98c254a8a9350b61d4cead60600f0e
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2014-07-15 14:37:10 +0900

    modified comments

--- ryu/services/protocols/bgp/bgpspeaker.py
+++ ryu/services/protocols/bgp/bgpspeaker.py
@@ -93,33 +93,37 @@ class PrefixList(object):
 
     We can create PrefixList object as follows.
 
-    * prefix_list = PrefixList('10.5.111.0/24',policy=PrefixList.POLICY_PERMIT)
+    * prefix_list = PrefixList('10.5.111.0/24',
+                               policy=PrefixList.POLICY_PERMIT)
 
-    ================ ======================================================
+    ================ ==================================================
     Attribute        Description
-    ================ ======================================================
+    ================ ==================================================
     prefix           A prefix used for out-filter
     policy           PrefixList.POLICY.PERMIT or PrefixList.POLICY_DENY
-    ge               Prefix length that will be applied on out-filter.
+    ge               Prefix length that will be applied out-filter.
                      ge means greater than or equal.
-    le               Prefix length that will be applied on out-filter.
+    le               Prefix length that will be applied out-filter.
                      le means less than or equal.
-    ================ ======================================================
+    ================ ==================================================
 
 
     For example, when PrefixList object is created as follows:
 
-    * p = PrefixList('10.5.111.0/24', policy=PrefixList.POLICY_DENY, ge=26, le=28)
+    * p = PrefixList('10.5.111.0/24',
+                     policy=PrefixList.POLICY_DENY, ge=26, le=28)
 
-    prefixes which matches 10.5.111.0/24 and its length matches from 26 to 28
-    will be filtered and stopped to send to neighbor because of POLICY_DENY.
-    If you specify POLICY_PERMIT, the path is sent to neighbor.
+    prefixes which matches 10.5.111.0/24 and its length matches
+    from 26 to 28 will be filtered and stopped to send to neighbor
+    because of POLICY_DENY. If you specify POLICY_PERMIT,
+    the path is sent to neighbor.
 
     If you don't want to send prefixes 10.5.111.64/26 and 10.5.111.32/27
     and 10.5.111.16/28, and allow to send other 10.5.111.0's prefixes,
     you can do it by specifying as follows;
 
-    * p = PrefixList('10.5.111.0/24', policy=PrefixList.POLICY_DENY, ge=26, le=28).
+    * p = PrefixList('10.5.111.0/24',
+                    policy=PrefixList.POLICY_DENY, ge=26, le=28).
 
     """
     POLICY_DENY = 0
@@ -136,10 +140,11 @@ class PrefixList(object):
         return cmp(self.prefix, other.prefix)
 
     def __repr__(self):
-        policy = 'PERMIT' if self._policy == self.POLICY_PERMIT else 'DENY'
-        return 'PrefixList(prefix=%s,policy=%s,ge=%s,le=%s)' % (self._prefix,
-                                                                policy,
-                                                                self._ge, self._le)
+        policy = 'PERMIT' \
+            if self._policy == self.POLICY_PERMIT else 'DENY'
+
+        return 'PrefixList(prefix=%s,policy=%s,ge=%s,le=%s)'\
+               % (self._prefix, policy, self._ge, self._le)
 
     @property
     def prefix(self):
@@ -158,6 +163,17 @@ class PrefixList(object):
         return self._le
 
     def evaluate(self, prefix):
+        """ This method evaluates the prefix. neighbor.
+
+        Returns this object's policy and the result of matching.
+        If the specified prefix matches this object's prefix and
+        ge and le condition,
+        this method returns true as the matching result.
+
+        ``prefix`` specifies the prefix. prefix must be string
+
+        """
+
         result = False
         length = prefix.length
         net = IPNetwork(prefix.formatted_nlri_str)
@@ -181,6 +197,13 @@ class PrefixList(object):
         return self.policy, result
 
     def clone(self):
+        """ This method clones PrefixList object.
+
+        Returns PrefixList object that has the same values with the
+        original one.
+
+        """
+
         return PrefixList(self.prefix,
                           policy=self._policy,
                           ge=self._ge,
