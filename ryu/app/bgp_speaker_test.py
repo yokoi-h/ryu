922774f193bc1edb183a253b9ba2be09eae4fb03
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2014-09-04 16:48:11 +0900

    add test script

---
+++ ryu/app/bgp_speaker_test.py
@@ -0,0 +1,29 @@
+import eventlet
+import time
+
+eventlet.monkey_patch()
+
+import logging
+import sys
+logging.basicConfig(level=logging.INFO)
+
+from ryu.services.protocols.bgp.bgpspeaker import BGPSpeaker
+
+def dump_remote_best_path_change(event):
+    print 'the best path changed:', event.remote_as, event.prefix,\
+        event.nexthop, event.is_withdraw
+
+if __name__ == "__main__":
+    speaker = BGPSpeaker(as_number=65000, router_id='10.0.0.7',
+                         best_path_change_handler=dump_remote_best_path_change, ssh_console=True,
+                         label_range=(1000,1999))
+
+    speaker.neighbor_add('172.16.6.101', 9598, enable_ipv4=True, enable_vpnv4=True)
+    speaker.neighbor_add('192.168.50.102', 65000, enable_ipv4=True, enable_vpnv4=True)
+    #speaker.vrf_add('65010:101', ['65010:101'], ['65010:101'])
+    eventlet.sleep(5)
+    #speaker.prefix_add('192.168.103.0/30', next_hop='0.0.0.0', route_dist='65010:101')
+    speaker.prefix_add('192.168.103.0/30', next_hop='0.0.0.0')
+
+    while True:
+        eventlet.sleep(5)
